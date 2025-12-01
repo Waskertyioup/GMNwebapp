ee368004dabf9bfc06fa739c4b83404feb08ebbc
Waskertyioup <abel.sierra_l@mail.udp.cl>
2025-12-01 14:04:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import bodyParser from "body-parser";
+import multer from "multer";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
 
 // Middleware
 app.use(cors());
@@ -19,16 +26,10 @@ app.use(express.static(path.join(__dirname, "public"))); // serve HTML, CSS, JS
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
   },
 });
-
-
-
-
-
-
 
 // === Endpoint to send mail ===
 app.post("/send-mail", async (req, res) => {
@@ -58,11 +59,55 @@ app.post("/send-mail", async (req, res) => {
   }
 });
 
+
+
+// === Dependencias ===
+
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+// === Endpoint para JOIN FORM ===
+app.post("/send-join-mail", upload.single("resume"), async (req, res) => {
+  const { nombre, correo, pais, linkedin, area } = req.body;
+  const file = req.file;
+
+  try {
+    await transporter.sendMail({
+      from: `"GMN Website" <stardustvstream@gmail.com>`,
+      to: `stardustvstream@gmail.com`,
+      subject: "Nueva postulación desde formulario TRABAJA",
+      html: `
+        <h3>Nueva Postulación</h3>
+        <p><b>Nombre:</b> ${nombre}</p>
+        <p><b>Correo:</b> ${correo}</p>
+        <p><b>País:</b> ${pais}</p>
+        <p><b>LinkedIn:</b> ${linkedin}</p>
+        <p><b>Área de interés:</b> ${area}</p>
+      `,
+      attachments: file
+        ? [
+            {
+              filename: file.originalname,
+              content: file.buffer,
+              contentType: file.mimetype,
+            },
+          ]
+        : [],
+    });
+
+    res.json({ ok: true, message: "Correo enviado correctamente" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ ok: false, message: "Error enviando correo" });
+  }
+});
+
+
 // === Serve index.html on root ===
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // === Start server ===
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
